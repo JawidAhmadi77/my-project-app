8ca29386625a7ddd519605322716100cc88f2b30
JawidAhmadi77 <ahmadijawid347@gmail.com>
2025-09-29 16:25:19 +0430

    Make the main structure for my project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 
-import Navbar from "./Components/Navbar";
+// import Navbar from "./Components/Navbar";
+import {logo} from "./assets/index"
+import Navbar from "./Components/Navbar"
+import styles, { layout } from "./Constants/Style"
 
 import React from 'react'
 
 export default function App() {
   return (
-    <div>
-      <Navbar/>
+    <>
+    <div className="w-full overflow-hidden text-white bg-primary">
+      <div className={` ${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+         <Navbar/>
+        
+        </div>
+      </div>
+
     </div>
+    </>
   )
 }
